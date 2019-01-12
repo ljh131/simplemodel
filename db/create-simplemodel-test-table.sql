26800993df9451437d6ed638070bc68328ea3a02
ljh131 <ljh131@gmail.com>
2019-01-12 15:54:06 +0900

    fix create table sql, etc

--- db/create-simplemodel-test-table.sql
+++ db/create-simplemodel-test-table.sql
@@ -1,16 +1,10 @@
-/*
-drop table companies;
-drop table employees;
-drop table products;
-*/
-
-create table companies (
+create table if not exists companies (
   id int(11) not null auto_increment primary key,
   name varchar(64) not null,
   created_at timestamp default now()
 );
 
-create table employees (
+create table if not exists employees (
   id int(11) not null auto_increment primary key,
   company_id int(11) null,
   name varchar(64) not null,
@@ -18,12 +12,10 @@ create table employees (
   created_at timestamp default now()
 );
 
-create table products (
+create table if not exists products (
   id int(11) not null auto_increment primary key,
   name varchar(64) not null,
   price int(11) not null,
   created_at timestamp default now(),
   deleted_at timestamp null default null
 );
-
-
