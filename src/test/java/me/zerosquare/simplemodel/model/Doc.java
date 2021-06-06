4051b72294de562eccdbb495b1a1902fc0db542b
ljh131 <ljh131@gmail.com>
2021-06-06 23:49:46 +0900

    - NEW: can access through aliased column name - FIX: skip key duplicate when same table joined - add new test tables

---
+++ src/test/java/me/zerosquare/simplemodel/model/Doc.java
@@ -0,0 +1,30 @@
+package me.zerosquare.simplemodel.model;
+
+import me.zerosquare.simplemodel.Model;
+import me.zerosquare.simplemodel.annotations.Column;
+import me.zerosquare.simplemodel.annotations.Table;
+
+@Table(name = "docs")
+public class Doc extends Model {
+  @Column
+  public Long id;
+
+  @Column(name = "user_id")
+  public Long userId;
+
+  @Column
+  public String title;
+
+  @Column
+  public String content;
+
+  public Doc() {
+  }
+
+  public Doc(Long userId, String title, String content) {
+    this.userId = userId;
+    this.title = title;
+    this.content = content;
+  }
+}
+
